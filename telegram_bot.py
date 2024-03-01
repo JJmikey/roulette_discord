1cc50b4ca995e2133b036f71996f3351f1118c9f
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-03-01 17:39:04 +0800

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,6 +3,7 @@ from google.generativeai.types import safety_types
 
 import os 
 
+from flask import Flask, request
 
 
 
@@ -14,13 +15,28 @@ api_key = os.getenv("GOOGLE_GEMINI_KEY")
 genai.configure(api_key=api_key)
 
 bot_token=os.getenv("TELEGRAM_BOT_TOKEN")
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="我是你的 Bot，很高興見到你！")
 
-updater = Updater(token=bot_token, use_context=True)
+application = Flask(__name__)
+bot = Bot(token=bot_token)
+dispatcher = Dispatcher(bot, None, workers=0)
 
-dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+@application.route(f'/{bot_token}', methods=['POST'])
+def index():
+    update = Update.de_json(request.get_json(), bot)
+    dispatcher.process_update(update)
+    return ''
+
+if __name__ == '__main__':
+    application.run(port=5000)
+
+
+
+
+
+
