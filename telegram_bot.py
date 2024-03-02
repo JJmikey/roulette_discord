2b4f9e659ec51abefefb01e58ba404e6f4d7eee8
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-03-02 14:27:46 +0800

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,16 +3,16 @@ from google.generativeai.types import safety_types
 
 import os 
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, logging
 
 
 
 
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, CommandHandler, CallbackContext
+from telegram.ext import CommandHandler, CallbackContext,MessageHandler
 
 
-# Initialize Gemini-Pro
+# Ini)tialize Gemini-Pro
 api_key = os.getenv("GOOGLE_GEMINI_KEY")
 genai.configure(api_key=api_key)
 
@@ -20,12 +20,15 @@ bot_token=os.getenv("TELEGRAM_BOT_TOKEN")
 
 
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.INFO)
 
 def start_callback(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="你好，我是你的 Bot！")
 
 
+def text_callback(update: Update, context: CallbackContext):
+    logging.info("執行 text_callback 函數")  # 這是新增的日誌語句
+    context.bot.send_message(chat_id=update.effective_chat.id, text="你好")
 
 # 初始化你的 bot 和 Dispatcher
 bot = Bot(bot_token)
@@ -37,6 +40,12 @@ dispatcher = Dispatcher(bot, None, workers=1)
 start_handler = CommandHandler('start', start_callback)  # 你需要定義 start_callback
 dispatcher.add_handler(start_handler)
 
+# 創建一個新的處理器來處理所有 text 信息
+text_handler = MessageHandler(Filters.text, text_callback)
+dispatcher.add_handler(text_handler)
+
+
+
 @app.route(f"/{bot_token}", methods=['POST'])
 def telegram_webhook():
     #建立一個 Update 物件，並使用進來的 JSON 進行填充
@@ -44,6 +53,9 @@ def telegram_webhook():
     #使用 Dispatcher 物件來處理該 Update
     dispatcher.process_update(update)
     return '', 200  # success status
+
+
+
 
 
 @app.route('/webhook_info', methods=['GET'])
